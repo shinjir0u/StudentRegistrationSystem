e99495e3c84338eb15f15ea0219cd764cba17d73
Shinjirou <htooagwai9@gmail.com>
2024-09-03 20:31:14 +0630

    Clean up the project
    The functionalities were tested with some test classes and no longer needed.

--- StudentRegistrationSystem/src/main/java/example/Employee.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class Employee extends Person {
-//	public Employee(int id, String name, String email) {
-//		setId(id);
-//		setName(name);
-//		setEmail(email);
-//	}
-}
